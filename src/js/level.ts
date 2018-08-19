4cf9eb5cfdbc5a8dcb5d2553a2cfd184bd53f83c
unreal <unreal9981@gmail.com>
2018-08-19 23:47:06 +0300

    level creating updated!

--- src/js/level.ts
+++ src/js/level.ts
@@ -7,9 +7,18 @@ const grass = PIXI.Texture.fromImage(imgs.grass);
 const ocean = PIXI.Texture.fromImage(imgs.ocean);
 const asphalt = PIXI.Texture.fromImage( imgs.asphalt);
 const Dust = PIXI.Texture.fromImage(imgs.dust)
-class Maps {
-  constructor(){
-    
+class Maps extends PIXI.Sprite{
+  constructor(img:object,count:number){
+    super(img);
+    let k:Maps;
+    for(var i = 0; i < 10*count; i++) {
+    k = new PIXI.Sprite(img);
+    k.anchor.set(0.5);
+    k.y = ((i % (1*count) * 50))+levelheight;
+    k.x = (Math.floor(i / (1*count)) * 50)+50;
+    container.addChild(k);
+    }
+    levelheight += count*50;
   }
 }
 
@@ -24,51 +33,32 @@ for (var i = 0; i < 144; i++) {
     container.addChild(levelDust);
 }
 let levelheight = 50;
+
 export let grassLines:Array<number> = [];
 export let asphaltLines:Array<number> = [];
 export let oceanLines:Array<number> = [];
 
 export function grassMap(count:number){
-  for(var i = 0; i < 10*count; i++) {
-    const grassMap = new PIXI.Sprite(grass);
-    grassMap.anchor.set(0.5);
-    grassMap.y = ((i % (1*count) * 50))+levelheight;
-    grassMap.x = (Math.floor(i / (1*count)) * 50)+50;
-    container.addChild(grassMap);
-    
-  }
-  levelheight += count*50;
+  const grassMap = new Maps(grass,count);
   for(let i = levelheight - ((count*50)-50); i <= levelheight; i+=50){
     grassLines.push(i);
   }
 }
 
 export function asphaltMap(count:number){
-  for(var i = 0; i < 10*count; i++) {
-    const asphaltMap = new PIXI.Sprite(asphalt);
-    asphaltMap.anchor.set(0.5);
-    asphaltMap.y = ((i % (1*count) * 50))+levelheight;
-    asphaltMap.x = (Math.floor(i / (1*count)) * 50)+50;
-    container.addChild(asphaltMap);
-  }
-  levelheight += count*50;
+  const asphaltMap = new Maps(asphalt,count);
   for(let i = levelheight - ((count*50)-50); i <= levelheight; i+=50){
     asphaltLines.push(i);
   }
 }
+
 export function oceanMap(count:number){
-  for(var i = 0; i < 10*count; i++) {
-    const oceanMap = new PIXI.Sprite(ocean);
-    oceanMap.anchor.set(0.5);
-    oceanMap.y = ((i % (1*count) * 50))+levelheight;
-    oceanMap.x = (Math.floor(i / (1*count)) * 50)+50;
-    container.addChild(oceanMap);
-  }
-  levelheight += count*50;
+  const oceanMap = new Maps(ocean,count);
   for(let i = levelheight - ((count*50)-50); i <= levelheight; i+=50){
     oceanLines.push(i);
   }
 }
 
 levelSettings();
+
 export default container;
--- src/js/settings.ts
+++ src/js/settings.ts
@@ -1,11 +1,10 @@
 import {grassMap ,asphaltMap, oceanMap} from "./level";
 export function levelSettings(){
-  grassMap(1);
+  grassMap(2);
   oceanMap(1);
   asphaltMap(1);
   oceanMap(2);
   oceanMap(1);
   asphaltMap(2);
-  grassMap(1);
   asphaltMap(1);
 }
